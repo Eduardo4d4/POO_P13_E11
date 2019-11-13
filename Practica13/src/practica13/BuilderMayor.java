cd360d03883e1b8efb65f477b30df41c193f8b97
DiegoVelazquez1005 <55148451+DiegoVelazquez1005@users.noreply.github.com>
2019-11-13 12:46:42 -0600

    ejercicio

---
+++ Practica13/src/practica13/BuilderMayor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica13;
+
+/**
+ *
+ * @author poo02alu13
+ */
+public class BuilderMayor {
+    
+}
